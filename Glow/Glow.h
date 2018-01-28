1f8bf1ca98f1c9e8741a73442b2d4fd83ade587e
Christian Otkjær <christianotkjaer@icloud.com>
2018-01-28 11:12:41 +0100

    Initial Commit

---
+++ Glow/Glow.h
@@ -0,0 +1,19 @@
+//
+//  Glow.h
+//  Glow
+//
+//  Created by Christian Otkjær on 28/01/2018.
+//  Copyright © 2018 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Glow.
+FOUNDATION_EXPORT double GlowVersionNumber;
+
+//! Project version string for Glow.
+FOUNDATION_EXPORT const unsigned char GlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glow/PublicHeader.h>
+
+
